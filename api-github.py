25ff9eb27e37cddca15ba05b20532cd16bb688b4
MurilloGodoi <murillogodoi13@hotmail.com>
2020-11-04 19:52:04 -0300

    Add Project

---
+++ api-github.py
@@ -0,0 +1,61 @@
+import requests
+import json
+
+class ListInformations():
+
+    def __init__(self, user):
+        self._user = user
+
+    def request_api(self):
+        response = requests.get(
+            f'https://api.github.com/users/{self._user}')    
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("User not found")
+            return response.status_code   
+            
+    def list_informations(self):
+        datas_api = self.request_api()
+        print("\n")
+        print("User: " + datas_api['login'])
+        print("User: " + int(datas_api['id']))
+        print("User: " + datas_api['avatar_url'])
+        print("User: " + datas_api['name'])
+        print("User: " + datas_api['company'])
+        print("User: " + datas_api['location'])
+        print("User: " + datas_api['bio'])
+        print("User: " + datas_api['public_repos'])
+        print("User: " + datas_api['followers'])
+        print("User: " + datas_api['following'])
+        print("User: " + datas_api['created_at'])
+        print("User: " + datas_api['updated_at'])
+
+class ListRepositories():
+    def __init__(self, user):
+        self._user = user
+
+    def request_api(self):
+        response = requests.get(
+            f'https://api.github.com/users/{self._user}/repos') 
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            print("User not found")
+            return response.status_code   
+            
+    def list_repositories(self):
+        print("\nList of Repositories:\n ")
+        datas_api = self.request_api()
+        for i in range(len(datas_api)):
+            print(datas_api[i]['name'])
+        print("\n")
+
+        
+user = input("Enter the name of the github user: ")
+informations = ListInformations(user)
+repositories = ListRepositories(user)
+informations.list_informations()
+repositories.list_repositories()
